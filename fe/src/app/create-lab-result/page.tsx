c4d8f95deecc4507bd00c5b12424b6891d4cd184
Emil Tonchev <emil.krassimirov@gmail.com>
2024-02-10 09:01:27 +0200

    Adds acordion with test type information on the create/edit lab result page

--- fe/src/app/create-lab-result/page.tsx
+++ fe/src/app/create-lab-result/page.tsx
@@ -36,6 +36,12 @@ import { format } from "date-fns";
 import { cn } from "@/lib/utils";
 import { Calendar } from "@/components/ui/calendar";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default function CreateLabResult() {
   const { data: session, status } = useSession();
@@ -151,193 +157,209 @@ export default function CreateLabResult() {
 
   return (
     <main className="flex flex-col items-center gap-16">
-      <div className="text-4xl mt-24">Create New Lab Result</div>
-      <div className="flex flex-col items-center gap-16">
-        <Form {...form}>
-          <form
-            className="max-w-md w-full flex flex-col gap-4"
-            onSubmit={form.handleSubmit(handleSubmit)}
-          >
-            <FormField
-              control={form.control}
-              name="physician"
-              render={({ field }) => {
-                return (
-                  <FormItem>
-                    <FormLabel>Physician</FormLabel>
-                    <FormControl>
-                      <Input {...field} placeholder="Physician" type="text" />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                );
-              }}
-            />
-            <FormField
-              control={form.control}
-              name="laboratory"
-              render={({ field }) => {
-                return (
-                  <FormItem>
-                    <FormLabel>Laboratory</FormLabel>
-                    <FormControl>
-                      <Input {...field} placeholder="Laboratory" type="text" />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                );
-              }}
-            />
-            <FormField
-              control={form.control}
-              name="date"
-              render={({ field }) => {
-                return (
-                  <FormItem>
-                    <FormLabel>Date</FormLabel>
-                    <FormControl>
-                      <Popover>
-                        <PopoverTrigger asChild>
-                          <FormControl>
-                            <Button
-                              variant={"outline"}
-                              className={cn(
-                                "w-full pl-3 text-left font-normal",
-                                !field.value && "text-muted-foreground",
-                              )}
-                            >
-                              {field.value ? (
-                                format(field.value, "yyyy-MM-dd")
-                              ) : (
-                                <span>Pick a date</span>
-                              )}
-                              <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                            </Button>
-                          </FormControl>
-                        </PopoverTrigger>
-                        <PopoverContent className="w-auto p-0" align="start">
-                          <Calendar
-                            mode="single"
-                            selected={field.value}
-                            onSelect={field.onChange}
-                            initialFocus
-                          />
-                        </PopoverContent>
-                      </Popover>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                );
-              }}
-            />
-            <div className="flex gap-2">
-              <Button
-                className="bg-slate-800 text-slate-50"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setDynamicFields([...dynamicFields, { testType: "", testValue: "" }]);
+      <div className="flex gap-64 w-1/2">
+        <div className="flex flex-col items-center gap-16 w-[70%]">
+          <div className="text-4xl mt-24">Create New Lab Result</div>
+          <Form {...form}>
+            <form
+              className="max-w-md w-full flex flex-col gap-4"
+              onSubmit={form.handleSubmit(handleSubmit)}
+            >
+              <FormField
+                control={form.control}
+                name="physician"
+                render={({ field }) => {
+                  return (
+                    <FormItem>
+                      <FormLabel>Physician</FormLabel>
+                      <FormControl>
+                        <Input {...field} placeholder="Physician" type="text" />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  );
                 }}
-              >
-                + Add Test
-              </Button>
-              <Button
-                onClick={(e) => {
-                  e.preventDefault();
-                  setDynamicFields(dynamicFields.slice(0, dynamicFields.length - 1));
+              />
+              <FormField
+                control={form.control}
+                name="laboratory"
+                render={({ field }) => {
+                  return (
+                    <FormItem>
+                      <FormLabel>Laboratory</FormLabel>
+                      <FormControl>
+                        <Input {...field} placeholder="Laboratory" type="text" />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  );
                 }}
-                disabled={dynamicFields.length === 1}
-                variant="destructive"
-              >
-                Remove Last Test
-              </Button>
-            </div>
-            {dynamicFields.map((field, index) => (
-              <div key={index} className="flex gap-6">
-                <FormField
-                  control={form.control}
-                  name={`testPairs[${index}].testType` as `testPairs.${number}.testType`}
-                  render={({ field }) => {
-                    return (
-                      <FormItem>
-                        <FormLabel>Blood Test Type</FormLabel>
-                        <FormControl>
-                          <Select
-                            onValueChange={(value) => {
-                              field.onChange(value);
-                              setSelectTestTypes([...selectTestTypes, value]);
-                            }}
-                          >
-                            <SelectTrigger className="w-52">
-                              <SelectValue placeholder="Select Blood Test" />
-                            </SelectTrigger>
-                            <SelectContent>
-                              <SelectGroup>
-                                <SelectLabel>Select Blood Test Type</SelectLabel>
-                                {bloodTestsTypes.map((test) => (
-                                  <SelectItem
-                                    disabled={selectTestTypes.includes(test.name)}
-                                    key={test.id}
-                                    value={test.name}
-                                  >
-                                    {test.name}
-                                  </SelectItem>
-                                ))}
-                              </SelectGroup>
-                            </SelectContent>
-                          </Select>
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    );
+              />
+              <FormField
+                control={form.control}
+                name="date"
+                render={({ field }) => {
+                  return (
+                    <FormItem>
+                      <FormLabel>Date</FormLabel>
+                      <FormControl>
+                        <Popover>
+                          <PopoverTrigger asChild>
+                            <FormControl>
+                              <Button
+                                variant={"outline"}
+                                className={cn(
+                                  "w-full pl-3 text-left font-normal",
+                                  !field.value && "text-muted-foreground",
+                                )}
+                              >
+                                {field.value ? (
+                                  format(field.value, "yyyy-MM-dd")
+                                ) : (
+                                  <span>Pick a date</span>
+                                )}
+                                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                              </Button>
+                            </FormControl>
+                          </PopoverTrigger>
+                          <PopoverContent className="w-auto p-0" align="start">
+                            <Calendar
+                              mode="single"
+                              selected={field.value}
+                              onSelect={field.onChange}
+                              initialFocus
+                            />
+                          </PopoverContent>
+                        </Popover>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  );
+                }}
+              />
+              <div className="flex gap-2">
+                <Button
+                  className="bg-slate-800 text-slate-50"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setDynamicFields([...dynamicFields, { testType: "", testValue: "" }]);
                   }}
-                />
-                <FormField
-                  control={form.control}
-                  name={`testPairs[${index}].testValue` as `testPairs.${number}.testValue`}
-                  render={({ field }) => {
-                    return (
-                      <FormItem>
-                        <FormLabel>Test Value</FormLabel>
-                        <FormControl>
-                          <Input
-                            {...field}
-                            placeholder="Test Value"
-                            type="number"
-                            step="any"
-                            onChange={(e) => {
-                              const value = parseFloat(e.target.value);
-                              field.onChange(value);
-                            }}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    );
+                >
+                  + Add Test
+                </Button>
+                <Button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setDynamicFields(dynamicFields.slice(0, dynamicFields.length - 1));
                   }}
-                />
+                  disabled={dynamicFields.length === 1}
+                  variant="destructive"
+                >
+                  Remove Last Test
+                </Button>
               </div>
-            ))}
-            <FormField
-              control={form.control}
-              name="note"
-              render={({ field }) => {
-                return (
-                  <FormItem>
-                    <FormLabel>Note</FormLabel>
-                    <FormControl>
-                      <Textarea {...field} placeholder="Note" />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                );
-              }}
-            />
-            {addNewLabResultError && (
-              <div className="text-sm text-red-600">Something went wrong, please try again.</div>
-            )}
-            <Button className="w-full bg-slate-800 text-slate-50">Create</Button>
-          </form>
-        </Form>
+              {dynamicFields.map((field, index) => (
+                <div key={index} className="flex gap-6">
+                  <FormField
+                    control={form.control}
+                    name={`testPairs[${index}].testType` as `testPairs.${number}.testType`}
+                    render={({ field }) => {
+                      return (
+                        <FormItem>
+                          <FormLabel>Blood Test Type</FormLabel>
+                          <FormControl>
+                            <Select
+                              onValueChange={(value) => {
+                                field.onChange(value);
+                                setSelectTestTypes([...selectTestTypes, value]);
+                              }}
+                            >
+                              <SelectTrigger className="w-52">
+                                <SelectValue placeholder="Select Blood Test" />
+                              </SelectTrigger>
+                              <SelectContent>
+                                <SelectGroup>
+                                  <SelectLabel>Select Blood Test Type</SelectLabel>
+                                  {bloodTestsTypes.map((test) => (
+                                    <SelectItem
+                                      disabled={selectTestTypes.includes(test.name)}
+                                      key={test.id}
+                                      value={test.name}
+                                    >
+                                      {test.name}
+                                    </SelectItem>
+                                  ))}
+                                </SelectGroup>
+                              </SelectContent>
+                            </Select>
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      );
+                    }}
+                  />
+                  <FormField
+                    control={form.control}
+                    name={`testPairs[${index}].testValue` as `testPairs.${number}.testValue`}
+                    render={({ field }) => {
+                      return (
+                        <FormItem>
+                          <FormLabel>Test Value</FormLabel>
+                          <FormControl>
+                            <Input
+                              {...field}
+                              placeholder="Test Value"
+                              type="number"
+                              step="any"
+                              onChange={(e) => {
+                                const value = parseFloat(e.target.value);
+                                field.onChange(value);
+                              }}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      );
+                    }}
+                  />
+                </div>
+              ))}
+              <FormField
+                control={form.control}
+                name="note"
+                render={({ field }) => {
+                  return (
+                    <FormItem>
+                      <FormLabel>Note</FormLabel>
+                      <FormControl>
+                        <Textarea {...field} placeholder="Note" />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  );
+                }}
+              />
+              {addNewLabResultError && (
+                <div className="text-sm text-red-600">Something went wrong, please try again.</div>
+              )}
+              <Button className="w-full bg-slate-800 text-slate-50">Create</Button>
+            </form>
+          </Form>
+        </div>
+        <div className="w-full">
+          <div className="text-4xl mt-24 text-center mb-12">Test Types Info</div>
+          <Accordion type="single" collapsible>
+            {bloodTestsTypes &&
+              bloodTestsTypes.length &&
+              bloodTestsTypes.map((testType) => (
+                <AccordionItem value="item-1">
+                  <AccordionTrigger>{testType.name}</AccordionTrigger>
+                  <AccordionContent>{testType.description}</AccordionContent>
+                  <AccordionContent>{testType.aliases}</AccordionContent>
+                </AccordionItem>
+              ))}
+          </Accordion>
+        </div>
       </div>
     </main>
   );
