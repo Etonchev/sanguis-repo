8c9880c83fd1554629e617e4adce5ce89ce09daf
Emil Tonchev <emil.krassimirov@gmail.com>
2024-02-11 10:05:05 +0200

    Adds Spring boot skeleton generated by Spring Initializr

---
+++ backend/src/main/java/uol/sanguis/SanguisApplication.java
@@ -0,0 +1,11 @@
+package uol.sanguis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanguisApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SanguisApplication.class, args);
+	}
+}
